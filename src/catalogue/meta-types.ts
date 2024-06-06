14a30a5b799cef1faf2b167a4f10d20880d3f3b9
夜夜 <vcde0000@gmail.com>
2024-06-06 19:44:35 +0800

    feat: Releases types

--- src/catalogue/meta-types.ts
+++ src/catalogue/meta-types.ts
@@ -4,6 +4,85 @@ export interface LangDict {
   [lang: string]: string
 }
 
+export interface Releases {
+  url: string
+  assets_url: string
+  upload_url: string
+  html_url: string
+  id: number
+  author: ReleasesAuthor
+  node_id: string
+  tag_name: string
+  target_commitish: string
+  name: string
+  draft: boolean
+  prerelease: boolean
+  created_at: string
+  published_at: string
+  assets: [ReleasesAsset]
+  tarball_url: string
+  zipball_url: string
+  body: string
+}
+
+export interface ReleasesAuthor {
+  login: string
+  id: number
+  node_id: string
+  avatar_url: string
+  gravatar_id: string
+  url: string
+  html_url: string
+  followers_url: string
+  following_url: string
+  gists_url: string
+  starred_url: string
+  subscriptions_url: string
+  organizations_url: string
+  repos_url: string
+  events_url: string
+  received_events_url: string
+  type: string
+  site_admin: boolean
+}
+
+export interface ReleasesAsset {
+  url: string
+  id: number
+  node_id: string
+  name: string
+  label: null
+  uploader: ReleasesUploader
+  content_type: string
+  state: string
+  size: number
+  download_count: number
+  created_at: string
+  updated_at: string
+  browser_download_url: string
+}
+
+export interface ReleasesUploader {
+  login: string
+  id: number
+  node_id: string
+  avatar_url: string
+  gravatar_id: string
+  url: string
+  html_url: string
+  followers_url: string
+  following_url: string
+  gists_url: string
+  starred_url: string
+  subscriptions_url: string
+  organizations_url: string
+  repos_url: string
+  events_url: string
+  received_events_url: string
+  type: string
+  site_admin: boolean
+}
+
 export interface Everything {
   last_update_time: string
   github(github: any): unknown
