7cbbba8589e77bfd6b06c1ae5340a0662f10a16a
Miyashooooo <cstrike.asia@gmail.com>
2024-11-06 03:31:44 +0800

    fix: github proxy switch

--- src/app/[locale]/plugin/[pluginId]/release/[version]/release-display.tsx
+++ src/app/[locale]/plugin/[pluginId]/release/[version]/release-display.tsx
@@ -9,7 +9,16 @@ import { PluginDependenciesAll } from "@/components/plugin/plugin-dependencies";
 import { TimeFormatted } from "@/components/time-formatted";
 import { prettySize } from "@/utils/unit-utils";
 import { Divider, Title } from "@mantine/core";
-import { IconCalendar, IconExternalLink, IconFileDescription, IconFileDownload, IconPackageImport, IconSparkles, IconTag, IconWeight } from "@tabler/icons-react";
+import {
+  IconCalendar,
+  IconExternalLink,
+  IconFileDescription,
+  IconFileDownload,
+  IconPackageImport,
+  IconSparkles,
+  IconTag,
+  IconWeight,
+} from "@tabler/icons-react";
 import { clsx } from "clsx";
 import { getTranslations } from "next-intl/server";
 import React from "react";
@@ -20,35 +29,53 @@ import { ProxyableDownloadButton } from "./proxyable-download-button";
 // import { SHA256 } from 'crypto-js';
 import { getInfo } from "@/catalogue/data";
 
-async function HashDisplay({kind, hash}: {kind: string, hash: string}) {
+async function HashDisplay({ kind, hash }: { kind: string; hash: string }) {
   return (
     <div className="flex gap-1 items-center align-center">
-      <p className="min-[300px]:min-w-[70px] font-bold text-color-attribute-entry">{kind}</p>
+      <p className="min-[300px]:min-w-[70px] font-bold text-color-attribute-entry">
+        {kind}
+      </p>
       <div className="overflow-hidden overflow-ellipsis whitespace-nowrap relative bottom-[1px]">
-        <code className="text-[14px] px-[4px] py-[1px] rounded bg-mantine-light-gray-background">{hash}</code>
+        <code className="text-[14px] px-[4px] py-[1px] rounded bg-mantine-light-gray-background">
+          {hash}
+        </code>
       </div>
-      <HashCopyButtonServer value={hash}/>
+      <HashCopyButtonServer value={hash} />
     </div>
-  )
+  );
 }
 
-async function DownloadSection({release, className}: { release: Releases, className?: string }) {
-  const t = await getTranslations('page.plugin.release')
-  const assets = release.assets[0]
+async function DownloadSection({
+  release,
+  className,
+}: {
+  release: Releases;
+  className?: string;
+}) {
+  const t = await getTranslations("page.plugin.release");
+  const assets = release.assets[0];
   // const encrypted = md5(assets.id.toString());
   // const hashSHA256 = SHA256(assets.id.toString()).toString()
-  const date = new Date(assets.created_at)
+  const date = new Date(assets.created_at);
   return (
-    <div className={clsx(className, 'flex flex-col gap-6')}>
+    <div className={clsx(className, "flex flex-col gap-6")}>
       <div className="flex flex-row flex-wrap gap-x-8 gap-y-4 mx-auto">
         <AttributeEntry label={t("version")} Icon={IconTag}>
           <p>{release.tag_name}</p>
         </AttributeEntry>
         <AttributeEntry label={t("date")} Icon={IconCalendar}>
-          <TimeFormatted date={date} format="LL" hoverFormat="LLLL" hoverOpenDelay={500}/>
+          <TimeFormatted
+            date={date}
+            format="LL"
+            hoverFormat="LLLL"
+            hoverOpenDelay={500}
+          />
         </AttributeEntry>
         <AttributeEntry label={t("size")} Icon={IconWeight}>
-          <ClickableTooltip label={`${assets.size} ${t('bytes')}`} openDelay={500}>
+          <ClickableTooltip
+            label={`${assets.size} ${t("bytes")}`}
+            openDelay={500}
+          >
             <p>{prettySize(assets.size)}</p>
           </ClickableTooltip>
         </AttributeEntry>
@@ -61,22 +88,22 @@ async function DownloadSection({release, className}: { release: Releases, classN
         <DownloadSectionButton
           className="max-sm:col-span-2 place-self-end"
           color="blue"
-          rightSection={<IconExternalLink size={16} stroke={1.6}/>}
+          rightSection={<IconExternalLink size={16} stroke={1.6} />}
           component="a"
           target="_blank"
           href={release.html_url}
         >
-          {t('visit_release')}
+          {t("visit_release")}
         </DownloadSectionButton>
 
         <div className="max-sm:col-span-2 flex flex-col gap-2">
           <ProxyableDownloadButton rawUrl={assets.browser_download_url}>
-            {t('download')}
+            {t("download")}
           </ProxyableDownloadButton>
         </div>
 
-        <div className="max-lg:hidden"/>
-        <GithubProxySwitchServer className="max-sm:justify-end max-lg:justify-center max-lg:col-span-2"/>
+        <div className="max-lg:hidden" />
+        {/* <GithubProxySwitchServer className="max-sm:justify-end max-lg:justify-center max-lg:col-span-2"/> */}
       </div>
 
       <div className="flex flex-col gap-1 sm:px-6">
@@ -84,25 +111,33 @@ async function DownloadSection({release, className}: { release: Releases, classN
         <HashDisplay kind="SHA256" hash={hashSHA256}/> */}
       </div>
     </div>
-  )
+  );
 }
 
-async function ReleaseDescription({description}: { description: string | undefined | null }) {
-  const t = await getTranslations('page.plugin.release')
+async function ReleaseDescription({
+  description,
+}: {
+  description: string | undefined | null;
+}) {
+  const t = await getTranslations("page.plugin.release");
   if (description === null || description === undefined) {
-    return <NoneText>{t('release_body_nothing')}</NoneText>
+    return <NoneText>{t("release_body_nothing")}</NoneText>;
   }
 
   return (
     <div>
-      <GfmMarkdown allowEmbedHtml>
-        {description || ''}
-      </GfmMarkdown>
+      <GfmMarkdown allowEmbedHtml>{description || ""}</GfmMarkdown>
     </div>
-  )
+  );
 }
 
-async function ContentDivider({children, ...rest}: { children: React.ReactNode, [_: string]: any }) {
+async function ContentDivider({
+  children,
+  ...rest
+}: {
+  children: React.ReactNode;
+  [_: string]: any;
+}) {
   return (
     <Divider
       className="w-full my-8"
@@ -111,42 +146,54 @@ async function ContentDivider({children, ...rest}: { children: React.ReactNode, 
       label={children}
       {...rest}
     />
-  )
+  );
 }
 
-export async function ReleaseDisplay({plugin, release}: { plugin: SimplePlugin, release: Releases }) {
-  const t = await getTranslations('page.plugin.release')
-  const isLatest = release.tag_name === plugin.latestRelease?.version
-  const assets = release.assets[0]
-  const info = await getInfo(plugin.github,plugin.package_name)
+export async function ReleaseDisplay({
+  plugin,
+  release,
+}: {
+  plugin: SimplePlugin;
+  release: Releases;
+}) {
+  const t = await getTranslations("page.plugin.release");
+  const isLatest = release.tag_name === plugin.latestRelease?.version;
+  const assets = release.assets[0];
+  const info = await getInfo(plugin.github, plugin.package_name);
   return (
     <>
       <div>
         <div className="flex gap-2 items-center justify-center">
-          <Title order={1} className="break-all">{assets.name}</Title>
-          {isLatest &&
-            <ClickableTooltip label={t('latest_label')} position="right">
-              <IconSparkles stroke={1.6} size={36} color="var(--mantine-color-teal-text)"/>
+          <Title order={1} className="break-all">
+            {assets.name}
+          </Title>
+          {isLatest && (
+            <ClickableTooltip label={t("latest_label")} position="right">
+              <IconSparkles
+                stroke={1.6}
+                size={36}
+                color="var(--mantine-color-teal-text)"
+              />
             </ClickableTooltip>
-          }
+          )}
         </div>
 
-        <DownloadSection className="mt-5" release={release}/>
+        <DownloadSection className="mt-5" release={release} />
 
         <ContentDivider>
-          <IconPackageImport size={14} stroke={1.5}/>
-          <span className="ml-1">{t('dependencies')}</span>
+          <IconPackageImport size={14} stroke={1.5} />
+          <span className="ml-1">{t("dependencies")}</span>
         </ContentDivider>
 
-        <PluginDependenciesAll meta={info}/>
+        <PluginDependenciesAll meta={info} />
 
         <ContentDivider>
-          <IconFileDescription size={14} stroke={1.5}/>
-          <span className="ml-1">{t('release_notes')}</span>
+          <IconFileDescription size={14} stroke={1.5} />
+          <span className="ml-1">{t("release_notes")}</span>
         </ContentDivider>
 
-        <ReleaseDescription description={release.body}/>
+        <ReleaseDescription description={release.body} />
       </div>
     </>
-  )
+  );
 }
--- src/app/[locale]/plugin/[pluginId]/releases/page.tsx
+++ src/app/[locale]/plugin/[pluginId]/releases/page.tsx
@@ -6,7 +6,13 @@ import { TimeFormatted } from "@/components/time-formatted";
 import { routes } from "@/site/routes";
 import { prettySize } from "@/utils/unit-utils";
 import { ScrollArea } from "@mantine/core";
-import { Icon, IconCalendar, IconFileDownload, IconTag, IconWeight } from "@tabler/icons-react";
+import {
+  Icon,
+  IconCalendar,
+  IconFileDownload,
+  IconTag,
+  IconWeight,
+} from "@tabler/icons-react";
 import { clsx } from "clsx";
 import { getTranslations, unstable_setRequestLocale } from "next-intl/server";
 import React from "react";
@@ -14,105 +20,119 @@ import { AssetDownloadButton } from "./asset-download-button";
 import { ReleaseRow } from "./release-row";
 
 interface IconTextProps {
-  className?: string
-  icon: Icon
-  size?: 'sm' | 'md'
-  children: React.ReactNode
+  className?: string;
+  icon: Icon;
+  size?: "sm" | "md";
+  children: React.ReactNode;
 }
 
 async function IconText(props: IconTextProps) {
-  const { size = 'sm' } = props
-  const isSmall = size === 'sm'
+  const { size = "sm" } = props;
+  const isSmall = size === "sm";
 
   return (
-    <div className={clsx(
-      props.className,
-      'flex flex-row gap-0.5 items-center',
-      isSmall && 'text-sm',
-    )}>
-      <props.icon size={isSmall ? 16 : 18} stroke={1.5}/>
+    <div
+      className={clsx(
+        props.className,
+        "flex flex-row gap-0.5 items-center",
+        isSmall && "text-sm"
+      )}
+    >
+      <props.icon size={isSmall ? 16 : 18} stroke={1.5} />
       {props.children}
     </div>
-  )
+  );
 }
 
-async function PluginContentReleases({plugin}: {plugin: AllOfAPlugin}) {
-  const t = await getTranslations('page.plugin.releases')
+async function PluginContentReleases({ plugin }: { plugin: AllOfAPlugin }) {
+  const t = await getTranslations("page.plugin.releases");
   let releases = undefined;
 
-  const info = await getPluginInfo(plugin.github)
+  const info = await getPluginInfo(plugin.github);
 
   // if(info.message && info.message.includes('rate limit')){
   //   return;
   // }
 
-  releases = info
-  if(!Array.isArray(releases)){
+  releases = info;
+  if (!Array.isArray(releases)) {
     return;
   }
 
   return (
     <ScrollArea scrollbars="x" className="w-full">
       <div className="min-w-[360px] mb-3">
-        {
-          releases.map((ri: Releases) => {
-            const version = ri.tag_name
-            const date = new Date(ri.created_at)
-            const href = routes.pluginRelease(plugin.package_name, version)
-            return (
-              <ReleaseRow key={version} href={href}>
-                <div className="flex flex-row items-center gap-4">
-                  <AssetDownloadButton
-                    className="place-self-start mt-2"
-                    href={ri.assets[0].browser_download_url}
-                    tooltip={t('download', {name: ri.assets[0].name, version})}
-                  />
+        {releases.map((ri: Releases) => {
+          const version = ri.tag_name;
+          const date = new Date(ri.created_at);
+          const href = routes.pluginRelease(plugin.package_name, version);
+          return (
+            <ReleaseRow key={version} href={href}>
+              <div className="flex flex-row items-center gap-4">
+                <AssetDownloadButton
+                  className="place-self-start mt-2"
+                  href={ri.assets[0].browser_download_url}
+                  tooltip={t("download", { name: ri.assets[0].name, version })}
+                />
 
-                  <div className={clsx(
+                <div
+                  className={clsx(
                     "grow",
                     "max-lg:flex max-lg:flex-col",
                     "lg:grid lg:grid-cols-[5fr_3fr] lg:items-center",
-                    "gap-x-2 gap-y-1",
-                  )}>
-                    <NaLink href={href}>
-                      <p className="font-bold">{ri.assets[0].name}</p>
-                      <IconText icon={IconTag} size="md">
-                        <p>{version}</p>
-                      </IconText>
-                    </NaLink>
+                    "gap-x-2 gap-y-1"
+                  )}
+                >
+                  <NaLink href={href}>
+                    <p className="font-bold">{ri.assets[0].name}</p>
+                    <IconText icon={IconTag} size="md">
+                      <p>{version}</p>
+                    </IconText>
+                  </NaLink>
 
-                    <NaLink href={href} className="flex flex-row lg:flex-col gap-x-3 gap-y-0.5">
-                      <IconText icon={IconCalendar}>
-                        <TimeFormatted date={date} format="LL" hoverFormat="LLL" hoverOpenDelay={500}/>
+                  <NaLink
+                    href={href}
+                    className="flex flex-row lg:flex-col gap-x-3 gap-y-0.5"
+                  >
+                    <IconText icon={IconCalendar}>
+                      <TimeFormatted
+                        date={date}
+                        format="LL"
+                        hoverFormat="LLL"
+                        hoverOpenDelay={500}
+                      />
+                    </IconText>
+                    <div className="flex gap-3">
+                      <IconText icon={IconWeight}>
+                        <p>{prettySize(ri.assets[0].size, 0)}</p>
                       </IconText>
-                      <div className="flex gap-3">
-                        <IconText icon={IconWeight}>
-                          <p>{prettySize(ri.assets[0].size, 0)}</p>
-                        </IconText>
-                        <IconText icon={IconFileDownload}>
-                          <p>{ri.assets[0].download_count}</p>
-                        </IconText>
-                      </div>
-                    </NaLink>
-                  </div>
+                      <IconText icon={IconFileDownload}>
+                        <p>{ri.assets[0].download_count}</p>
+                      </IconText>
+                    </div>
+                  </NaLink>
                 </div>
-              </ReleaseRow>
-            )
-          })
-        }
+              </div>
+            </ReleaseRow>
+          );
+        })}
       </div>
     </ScrollArea>
-  )
+  );
 }
 
-export default async function Page({params: {pluginId, locale}}: { params: { pluginId: string, locale: string } }) {
-  unstable_setRequestLocale(locale)
-  const plugin = await getPluginOr404(pluginId)
+export default async function Page({
+  params: { pluginId, locale },
+}: {
+  params: { pluginId: string; locale: string };
+}) {
+  unstable_setRequestLocale(locale);
+  const plugin = await getPluginOr404(pluginId);
 
   return (
     <div className="flex flex-col gap-1">
-      <PluginContentReleases plugin={plugin}/>
-      <GithubProxySwitchServer className="place-self-end" />
+      <PluginContentReleases plugin={plugin} />
+      {/* <GithubProxySwitchServer className="place-self-end" /> */}
     </div>
-  )
+  );
 }
